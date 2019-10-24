f7f49e4d746e5aebefc886ffd474a60613cec255
wraikny <writtenknown@gmail.com>
2019-10-24 18:31:55 +0900

    fix copy_cbg

--- scripts/copy_cbg.py
+++ scripts/copy_cbg.py
@@ -3,4 +3,13 @@ import os, shutil, ntpath, glob
 # move to source directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-shutil.copytree('../Core/scripts/bindings', './bindings')
+targetPath = '../Core/scripts/bindings'
+outputPath = "./bindings"
+
+if os.path.exists(outputPath):
+    print('Directory {} exists'.format(outputPath))
+    shutil.rmtree(outputPath)
+    print('Removed directory {}'.format(outputPath))
+
+shutil.copytree(targetPath, outputPath)
+print('Copied directory {} to {}'.format(targetPath, outputPath))
