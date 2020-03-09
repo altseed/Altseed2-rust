9d0bd7e5989ccea02036223d28d69aa93c0ade98
wraikny <writtenknown@gmail.com>
2020-03-10 00:15:20 +0900

    build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,28 @@
-fn main() {
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let flag = if Ok("release".to_owned()) == std::env::var("PROFILE") {
+        "RELEASE"
+    } else {
+        "DEBUG"
+    };
+
+    let dll_name = match std::env::consts::OS {
+        "windows" => "Altseed_Core.dll",
+        "macos" => "libAltseed_Core.dylib",
+        _ => "libAltseed_Core.so",
+    };
+
+    fs::copy(
+        format!("Core/build/DEBUG/{}", dll_name),
+        format!("./{}", dll_name),
+    )?;
+
+    fs::copy(
+        format!("Core/build/{}/{}", flag, dll_name),
+        format!("target/{}/{}", flag, dll_name),
+    )?;
     println!("cargo:rustc-link-search=native=.");
+
+    Ok(())
 }
