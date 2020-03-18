34de2abc8dddc9a1ef0ddbdbc32adf37936490d7
wraikny <writtenknown@gmail.com>
2020-03-19 03:16:30 +0900

    remove unused script

--- scripts/copy_dylib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`
-
-cp ../Core/build/DEBUG/libAltseed_Core.dylib ../target/debug/.
-cp ../Core/build/DEBUG/libAltseed_Core.dylib ..
-cp ../Core/build/RELEASE/libAltseed_Core.dylib ../target/release/.
