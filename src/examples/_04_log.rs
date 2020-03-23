12f43bf64f5d70f6ade1334373f6d3867cbc951d
wraikny <writtenknown@gmail.com>
2020-03-24 01:15:37 +0900

    generate example

--- src/examples/_04_log.rs
+++ src/examples/_04_log.rs
@@ -9,28 +9,21 @@
 //!     // Altseedを初期化します。 ?演算子を利用してError時に早期終了します。
 //!     let engine = Engine::initialize("log", 800, 600)?;
 //!
-//!     engine.log().borrow_mut().info(
-//!         LogCategory::User,
-//!         "ログ機能を使ってコンソールとファイルにログを出力できます",
-//!     );
-//!     engine.log().borrow_mut().warn(
-//!         LogCategory::User,
-//!         "重要度に応じてログの種類を切り替えられます",
-//!     );
 //!     engine
 //!         .log()
 //!         .borrow_mut()
-//!         .trace(LogCategory::User, "トレース");
-//!     engine.log().borrow_mut().info(LogCategory::User, "情報");
+//!         .info("コンソールとファイルにログを出力できます");
+//!
 //!     engine
 //!         .log()
 //!         .borrow_mut()
-//!         .debug(LogCategory::User, "デバッグ");
-//!     engine.log().borrow_mut().error(LogCategory::User, "エラー");
-//!     engine
-//!         .log()
-//!         .borrow_mut()
-//!         .critical(LogCategory::User, "致命的");
+//!         .warn("重要度に応じて種類を変更できます");
+//!
+//!     engine.log().borrow_mut().trace("トレース");
+//!     engine.log().borrow_mut().info("情報");
+//!     engine.log().borrow_mut().debug("デバッグ");
+//!     engine.log().borrow_mut().error("エラー");
+//!     engine.log().borrow_mut().critical("致命的");
 //!
 //!     // 正常終了
 //!     Ok(())
--- src/examples/mod.rs
+++ src/examples/mod.rs
@@ -1,9 +1,6 @@
 // Auto-generated. Do not modify.
 // このファイルは自動生成されたものです。変更しないでください。
 
-//! # Link
-//! [Documents](../documents/index.html)
-
 pub mod _00_hello_world;
 pub mod _01_sprite;
 pub mod _02_text;
